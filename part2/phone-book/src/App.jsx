3a4523c8347e8cf640a7ab522a74214abe913cd1
sinname <nexxuz2017@gmail.com>
2025-03-08 17:00:31 -0500

    axios and db.json changed number for tel

--- part2/phone-book/src/App.jsx
+++ part2/phone-book/src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Filter } from "./components/Filter";
 import { PersonForm } from "./components/PersonForm";
 import { Persons } from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", tel: "040-123456", id: 1 },
-    { name: "Ada Lovelace", tel: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", tel: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", tel: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("juan");
   const [number, setNewNumber] = useState("");
   const [showAll, setShowAll] = useState("");
 
-  console.log(persons);
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((res) => {
+      const response = res.data;
+      console.log(response);
+      setPersons(response);
+    });
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
