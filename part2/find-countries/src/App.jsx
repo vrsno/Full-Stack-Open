8fffc47bea90067642ab0c4f01ef7fb079efa4b6
sinname <nexxuz2017@gmail.com>
2025-03-13 20:20:45 -0500

    filter countries no finished

--- part2/find-countries/src/App.jsx
+++ part2/find-countries/src/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [country, setCountry] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (!country.trim()) {
+      setCountries([]);
+      setError(false);
+      return;
+    }
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://studies.cs.helsinki.fi/restcountries/api/all`
+        );
+        console.log(response.data);
+
+        const filteredCountries = response.data.filter((c) =>
+          c.name.common.toLowerCase().includes(country.toLowerCase())
+        );
+
+        if (filteredCountries.length > 10) {
+          setCountries([]);
+          setError("Too many matches, please refine your search.");
+        } else {
+          setCountries(filteredCountries);
+          setError(false);
+        }
+      } catch (err) {
+        console.log(err);
+
+        setError("Error fetching data");
+        setCountries([]);
+      }
+    };
+    // para no llamar multiples veces a la api
+
+    const delay = setTimeout(fetchData, 500);
+    return () => clearTimeout(delay);
+  }, [country]);
+
+  const handleCountryChange = (e) => {
+    setCountry(e.target.value);
+    setError(false);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Find country</p>
+        <input type="text" value={country} onChange={handleCountryChange} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {countries.length === 1 ? (
+        <div>
+          <h2>{countries[0].name.common}</h2>
+          <p>Capital: {countries[0].capital}</p>
+          <p>Area: {countries[0].area}</p>
+          <h2>Languages:</h2>
+          <ul>
+            {countries[0].languages &&
+              Object.values(countries[0].languages).map((lang, index) => (
+                <li key={index}>{lang}</li>
+              ))}
+          </ul>
+
+          <img
+            src={countries[0].flags.svg}
+            alt={`Flag of ${countries[0].name.common}`}
+            width="150"
+          />
+        </div>
+      ) : (
+        <ul>
+          {countries.map((c) => (
+            <li key={c.name.common}>{c.name.common}</li>
+          ))}
+        </ul>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
